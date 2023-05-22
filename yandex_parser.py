dd9280442b12b4609990ffaf9a91fbf26bcaa1d3
Ulquiorra <rodiontrig@gmail.com>
2023-05-22 13:47:15 +0300

    Dockerfile

--- yandex_parser.py
+++ yandex_parser.py
@@ -199,3 +199,4 @@ for i in range(len(me.album_tracks)):
 # list=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
 # for i in range(0+MyPerson.count*MyPerson.page-5,MyPerson.count*MyPerson.page):
 #     print(list[i])
+
